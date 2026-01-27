e466a68bfa12368d6b300a646f0e798d25078978
Laura FitzSimons <ltyf98@gmail.com>
2026-01-27 11:02:25 -0600

    Update script.js

--- HW3/script.js
+++ HW3/script.js
@@ -1,3 +1,19 @@
-console.log ("Stardew Valley. ") 
 
+console.log ("Favorite Websites")
+console.log ("YouTube") 
+console.log ("Instagram")
+console.log ("Blick (art shop)")
 
+console.log ("Favorite Games")
+console.log ("Stardew Valley") 
+console.log ("Whisper of the House")
+console.log ("Otrio (board game)")
+
+console.log ("Favorite Artists and Their Cultural Importance")
+console.log ("K-pop group Seventeen – global K-pop group and Goodwill Ambassador for Youth") 
+console.log ("")
+console.log ("")
+
+prompt("What is your favorite animal?")
+console.log ("")
+console.log ("")
