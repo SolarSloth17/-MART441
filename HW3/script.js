c6b9c450a58fa35c7bd842773f2662c3ff6560ab
Laura FitzSimons <ltyf98@gmail.com>
2026-01-28 09:01:22 -0600

    Update script.js

--- HW3/script.js
+++ HW3/script.js
@@ -11,8 +11,8 @@ console.log ("Otrio (board game)")
 
 console.log ("Favorite Artists and Their Cultural Importance")
 console.log ("K-pop group Seventeen – global K-pop group and Goodwill Ambassador for Youth") 
-console.log ("")
-console.log ("")
+console.log ("Lin-Manuel Miranda – Best known for Broadway Hamilton and in the Heights. He is also known for doing soundtracks for films like Moana and Encanto.  ")
+console.log ("J-pop group Snow Man – One of the most prominent J-pop groups")
 
 prompt("What is your favorite animal?")
 console.log ("")
