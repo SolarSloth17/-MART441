a48dafaab0ed2a18338d1eca199f945d9c704b9b
Laura FitzSimons <ltyf98@gmail.com>
2026-02-19 12:20:07 -0600

    Update script.js

--- HW6/script.js
+++ HW6/script.js
@@ -1,1 +1,18 @@
+var imageTags = ["image1", "image2", "image3", "image4","image5","image6", "image7", "image8", "image9"];
 
+var blankImagePath = "images/gofish.jpg";
+
+var actualImages = new Array();
+
+function printBlanks()
+{
+
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+       
+    
+    
+}
