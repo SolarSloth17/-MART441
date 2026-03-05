1e056709b82e63c750855abd8666cb69ff6eec37
Laura FitzSimons <ltyf98@gmail.com>
2026-03-04 19:19:54 -0600

    Update script.js

--- HW8/script.js
+++ HW8/script.js
@@ -1,1 +1,19 @@
+var myViewFinderArray = new Array();
 
+class ViewFinder {
+    constructor(title, image, description, author, imageYear) {
+        this.title = title;
+        this.image = image;
+        this.description = description;
+        this.author = author;
+        this.imageYear = imageYear;
+    }
+
+    toString() {
+        return `<h2>${this.title}</h2>
+                <img src='${this.image}' alt='${this.title}' width='300'>
+                <p><strong>Description:</strong> ${this.description}</p>
+                <p><strong>Author:</strong> ${this.author}</p>
+                <p><strong>Year:</strong> ${this.imageYear}</p>`;
+    }
+}
