e92f02aa3be48a66c9301767d1b77b02b2ae0c43
Laura FitzSimons <ltyf98@gmail.com>
2026-03-16 09:36:25 -0500

    Add functions to switch image, text, and shape

--- HW9/Scripts/sscript.js
+++ HW9/Scripts/sscript.js
@@ -24,3 +24,27 @@ class ElementInfo {
         return this.selector + ":" + this.content;
     }
 }
+  function switchImage() {
+        $(imageSelector).fadeOut(1000, function () {
+            currentImageIndex = (currentImageIndex + 1) % allImages.length;
+            $(imageSelector).attr("src", allImages[currentImageIndex].theContent);
+            $(imageSelector).fadeIn(1000);
+        });
+    }
+
+    function switchText() {
+        $(textSelector).fadeOut(1000, function () {
+            currentTextIndex = (currentTextIndex + 1) % allTexts.length;
+            $(textSelector).text(allTexts[currentTextIndex].theContent);
+            $(textSelector).fadeIn(1000);
+        });
+    }
+
+    function switchShape() {
+        $(shapeSelector).fadeOut(1000, function () {
+            currentShapeIndex = (currentShapeIndex + 1) % allShapes.length;
+            $(shapeSelector).removeClass().addClass("elementInfo " + allShapes[currentShapeIndex].theContent);
+            $(shapeSelector).fadeIn(1000);
+        });
+    }
+
