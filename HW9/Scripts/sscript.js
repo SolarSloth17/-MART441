4d81f079324d74467a0866825076478464d2826b
Laura FitzSimons <ltyf98@gmail.com>
2026-03-16 09:35:23 -0500

    Update sscript.js

--- HW9/Scripts/sscript.js
+++ HW9/Scripts/sscript.js
@@ -1,1 +1,26 @@
+var imageSelector = "#artImage";
+var textSelector = "#artText";
+var shapeSelector = "#artShape";
 
+var allImages = new Array();
+var allTexts = new Array();
+var allShapes = new Array();
+
+class ElementInfo {
+    constructor(selector, content) {
+        this.selector = selector;
+        this.content = content;
+    }
+
+    get theSelector() {
+        return this.selector;
+    }
+
+    get theContent() {
+        return this.content;
+    }
+
+    toString() {
+        return this.selector + ":" + this.content;
+    }
+}
