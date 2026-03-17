d18ac49174a081eed73e98ea021b9e167e427580
Laura FitzSimons <ltyf98@gmail.com>
2026-03-17 11:08:49 -0500

    Update sscript.js

--- HW9/Scripts/sscript.js
+++ HW9/Scripts/sscript.js
@@ -6,6 +6,26 @@ var allImages = new Array();
 var allTexts = new Array();
 var allShapes = new Array();
 
+function initializeArray() {
+    allImages.push(new ElementInfo("#artImage", "images/image1.jpg"));
+    allImages.push(new ElementInfo("#artImage", "images/image2.jpg"));
+    allImages.push(new ElementInfo("#artImage", "images/image3.jpg"));
+
+    allShapes.push(new ElementInfo("#artShape", "square"));
+    allShapes.push(new ElementInfo("#artShape", "circle"));
+    allShapes.push(new ElementInfo("#artShape", "triangle"))
+
+    allTexts.push(new ElementInfo("#artText", "Australia is wider than the moon, measuring almost 4,000 km in diameter from east to west."));
+    allTexts.push(new ElementInfo("#artText", "In the 1830s, ketchup was sold as a medicine to treat diarrhea, indigestion, and jaundice."));
+    allTexts.push(new ElementInfo("#artText", "It's illegal to own just one guinea pig in Switzerland."));
+;
+    $(document).ready(function(){
+    initializeArray();
+    console.log(allDogs.length);
+    console.log(allDogs[0].toString());
+    console.log(allDogs[0].theSelector);
+    console.log(allDogs[0].theImagePath);
+
 class ElementInfo {
     constructor(selector, content) {
         this.selector = selector;
