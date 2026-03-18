184157bf60f31ff3c566b351330c00066fd08ec2
Laura FitzSimons <ltyf98@gmail.com>
2026-03-18 10:20:12 -0500

    Update sscript.js

--- HW9/Scripts/sscript.js
+++ HW9/Scripts/sscript.js
@@ -1,70 +1,58 @@
-var imageSelector = "#artImage";
-var textSelector = "#artText";
-var shapeSelector = "#artShape";
-
-var allImages = new Array();
-var allTexts = new Array();
-var allShapes = new Array();
-
-function initializeArray() {
-    allImages.push(new ElementInfo("#artImage", "images/image1.jpg"));
-    allImages.push(new ElementInfo("#artImage", "images/image2.jpg"));
-    allImages.push(new ElementInfo("#artImage", "images/image3.jpg"));
-
-    allShapes.push(new ElementInfo("#artShape", "square"));
-    allShapes.push(new ElementInfo("#artShape", "circle"));
-    allShapes.push(new ElementInfo("#artShape", "triangle"))
-
-    allTexts.push(new ElementInfo("#artText", "Australia is wider than the moon, measuring almost 4,000 km in diameter from east to west."));
-    allTexts.push(new ElementInfo("#artText", "In the 1830s, ketchup was sold as a medicine to treat diarrhea, indigestion, and jaundice."));
-    allTexts.push(new ElementInfo("#artText", "It's illegal to own just one guinea pig in Switzerland."));
-;
-    $(document).ready(function(){
-    initializeArray();
-    console.log(allDogs.length);
-    console.log(allDogs[0].toString());
-    console.log(allDogs[0].theSelector);
-    console.log(allDogs[0].theImagePath);
-
-class ElementInfo {
-    constructor(selector, content) {
+var ricedishSelector = "#ricedish";
+var allRiceDish = new Array();
+class ricedishInfo{
+    constructor(selector, imagePath)
+    {
         this.selector = selector;
-        this.content = content;
+        this.imagePath = imagePath;
     }
 
-    get theSelector() {
+    get theSelector(){
         return this.selector;
     }
 
-    get theContent() {
-        return this.content;
+    get theImagePath(){
+        return this.imagePath;
     }
 
-    toString() {
-        return this.selector + ":" + this.content;
+    toString()
+    {
+        return this.selector + ":" + this.imagePath; 
     }
 }
-  function switchImage() {
-        $(imageSelector).fadeOut(1000, function () {
-            currentImageIndex = (currentImageIndex + 1) % allImages.length;
-            $(imageSelector).attr("src", allImages[currentImageIndex].theContent);
-            $(imageSelector).fadeIn(1000);
-        });
-    }
 
-    function switchText() {
-        $(textSelector).fadeOut(1000, function () {
-            currentTextIndex = (currentTextIndex + 1) % allTexts.length;
-            $(textSelector).text(allTexts[currentTextIndex].theContent);
-            $(textSelector).fadeIn(1000);
-        });
-    }
+function initializeArray()
+{
 
-    function switchShape() {
-        $(shapeSelector).fadeOut(1000, function () {
-            currentShapeIndex = (currentShapeIndex + 1) % allShapes.length;
-            $(shapeSelector).removeClass().addClass("elementInfo " + allShapes[currentShapeIndex].theContent);
-            $(shapeSelector).fadeIn(1000);
-        });
-    }
+    var ricedish = new ricedishInfo("#ricedish", "images/dog.jpg");
+    ricedish.push(ricedish);
 
+}
+$(document).ready(function(){
+    initializeArray();
+    console.log(allRiceDish.length);
+    console.log(allRiceDish[0].toString());
+    console.log(allRiceDish[0].theSelector);
+    console.log(allRiceDish[0].theImagePath);
+    
+    //$(allDogs[0].theSelector).src = allDogs[0].theImagePath;
+    //$("#my_image").attr("src","second.jpg");
+    $(allRiceDish[0].theSelector).attr("src", allRiceDish[0].theImagePath);
+
+    $("button").click(function(){
+       
+        $(".stuff").fadeOut();
+
+        $("#third").toggle();
+           setInterval(moveSquare, 1000);
+        
+        $(allDogs[0].theSelector).fadeOut().fadeIn();
+        
+    });
+    
+});
+
+function moveSquare()
+{
+    $("#square").animate({left:250}).animate({top:400}).animate({left:0}).animate({top:300});
+}
