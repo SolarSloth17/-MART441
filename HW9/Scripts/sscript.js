849d03ae33c124a535fabc75c335f99d8e3125fe
Laura FitzSimons <ltyf98@gmail.com>
2026-03-22 12:36:13 -0500

    Update sscript.js

--- HW9/Scripts/sscript.js
+++ HW9/Scripts/sscript.js
@@ -1,104 +1,82 @@
-var ricedishSelector = "#ricedish";
 var allRiceDish = new Array();
-class ricedishInfo{
-    constructor(selector, imagePath)
-    {
+
+class ricedishInfo {
+    constructor(selector, imagePath) {
         this.selector = selector;
         this.imagePath = imagePath;
     }
-
-    get theSelector(){
-        return this.selector;
-    }
-
-    get theImagePath(){
-        return this.imagePath;
-    }
-
-    toString()
-    {
-        return this.selector + ":" + this.imagePath; 
-    }
+    get theSelector() { return this.selector; }
+    get theImagePath() { return this.imagePath; }
+    toString() { return this.selector + ":" + this.imagePath; }
 }
 
-function initializeArray()
-{
+const riceImages = ["Images/kimbap.jpg", "Images/rice_balls.webp", "Images/sushi.jpeg"];
+const riceTexts = [
 
-    var ricedish = new ricedishInfo("#ricedish", "images/dog.jpg");
-    ricedish.push(ricedish);
+    "Kimbap is a Korean dish but is different from sushi",
+    "Rice balls are a portable rice dish that is formed into triangles",
+    "Sushi is a Japanese dish that often includes rice with a variety of ingredients"
+];
 
+let currentIndex = 0;
+
+function initializeArray() {
+    var ricedish = new ricedishInfo("#mainImage", "Images/kimbap.jpg");
+    allRiceDish.push(ricedish);
 }
-$(document).ready(function(){
+
+
+function swapImage() {
+    $("#mainImage, #dishDescription").fadeOut(500, function() {
+        if ($(this).is("#mainImage")) {
+            currentIndex = (currentIndex + 1) % riceImages.length;
+
+            $("#mainImage").attr("src", riceImages[currentIndex]);
+            $("#dishDescription").text(riceTexts[currentIndex]);
+
+            let randomX = Math.floor(Math.random() * 300);
+            let randomY = Math.floor(Math.random() * 200);
+            $("#mainImage").css({ left: randomX + "px", top: randomY + "px" });
+
+            $("#mainImage, #dishDescription").fadeIn(500, function() {
+                
+                $("#mainImage").animate({ left: "+=50px", top: "+=30px" }, 2000);
+                
+                $("#sushi").fadeIn(500).animate({ left: "+=200px" }, 5000, "linear");
+            });
+        }
+    });
+}
+
+function moveShapes() {
+    $("#square").animate({ left: "200px", top: "400px" }, 1000);
+    $("#circle").animate({ left: "400px", top: "500px" }, 1000).fadeOut(1000);
+    $("#triangle").animate({ left: "100px", top: "500px" }, 1000).fadeIn(1000);
+}
+
+function movetext() {
+  $("#third").fadeIn(500).animate({left: "300px"}, 1000);
+}
+
+$(document).ready(function() {
     initializeArray();
-    console.log(allRiceDish.length);
-    console.log(allRiceDish[0].toString());
-    console.log(allRiceDish[0].theSelector);
-    console.log(allRiceDish[0].theImagePath);
-
-      const images = [
-        "images/image1.jpg",
-        "images/image2.jpg",
-        "images/image3.jpg"
-      ];
-  
-    const texts = [
-      "Imagination is the gateway of reality",
-      "Your senses tell you what is, not what can be",
-      "Look inward"
-    ];
-  
-    const shapes = ["circle", "square", "triangle"];
-  
-    let imageIndex = 0;
-    let textIndex = 0;
-    let shapeIndex = 0;
-  
-    // === Animate Image ===
-    function switchImage() {
-      $("#image").fadeOut(1000, function () {
-        $("#image").attr("src", images[imageIndex]);
-        moveRandom("#image");
-        $("#image").fadeIn(1000);
-        imageIndex = (imageIndex + 1) % images.length;
-      });
-    }
-  
-    // === Animate Text ===
-    function switchText() {
-      $("#text").fadeOut(1000, function () {
-        $("#text").text(texts[textIndex]);
-        moveRandom("#text");
-        $("#text").fadeIn(1000);
-        textIndex = (textIndex + 1) % texts.length;
-      });
-    }
-  
-    // === Animate Shape ===
-    function switchShape() {
-      const shape = shapes[shapeIndex];
-      const shapeDiv = $("#shape");
-  
-      // Reset shape styles
-      shapeDiv.removeClass().css({ display: "none" });
     
-    //$(allDogs[0].theSelector).src = allDogs[0].theImagePath;
-    //$("#my_image").attr("src","second.jpg");
     $(allRiceDish[0].theSelector).attr("src", allRiceDish[0].theImagePath);
 
-    $("button").click(function(){
-       
-        $(".stuff").fadeOut();
-
-        $("#third").toggle();
-           setInterval(moveSquare, 1000);
-        
-        $(allDogs[0].theSelector).fadeOut().fadeIn();
-        
+    $("#movesquare").click(function() {
+        $("#square").animate({left: "+=100px", top: "+=50px"}, 500);
     });
-    
-});
 
-function moveSquare()
-{
-    $("#square").animate({left:250}).animate({top:400}).animate({left:0}).animate({top:300});
-}
+    $("#movecircle").click(function() {
+        $("#circle").animate({left: "+=50px", top: "-=20px"}, 500);
+    });
+
+    $("#movetriangle").click(function() {
+        $("#triangle").animate({left: "300px"}, 1000);
+    });
+
+
+    setInterval(swapImage, 6000);
+    setTimeout(moveShapes, 2000); 
+    setTimeout(movetext, 3000);
+});
