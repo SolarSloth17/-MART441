8e1dd29137e54c4cfcb5f934dd41cf0ae855985a
Laura FitzSimons <ltyf98@gmail.com>
2026-03-26 10:45:21 -0500

    Update ajax.js

--- HW11/scripts/ajax.js
+++ HW11/scripts/ajax.js
@@ -1,1 +1,10 @@
+ $(document).ready(function () {
+            $("button").click(function () {
+                $("#holidayInformation").load("data/bikeInfo.txt", fadeText);
+            });
+        });
 
+        function fadeText()
+        {
+            $("#holidayInformation").fadeOut("slow").fadeIn("slow");
+        }
