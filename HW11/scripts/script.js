49500c9bfe6a688fde8a63d5a1e78ecf8650412d
Laura FitzSimons <ltyf98@gmail.com>
2026-03-26 11:06:00 -0500

    Update script.js

--- HW11/scripts/script.js
+++ HW11/scripts/script.js
@@ -1,1 +1,18 @@
+ let holiday = {
+                "name": "Easter Sunday",
+                "date": "Variable (March/April)",
+                "type": "Religious"
+            }
+        
+        $(function () {
+            $("button").click(function () {
+                showHolidayInfo();
+            });
 
+        });
+       
+        function showBikeInfo()
+            {
+                $("#HolidayInformation").html("Name: " + holiday.name
+                + "<br>date:" + holiday.date+ "<br>type" + holiday.type);
+            }
