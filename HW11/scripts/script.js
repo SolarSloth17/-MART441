e12294c00e5a2d3e6e5ddc08c45a1ad0c3d1c9e6
Laura FitzSimons <ltyf98@gmail.com>
2026-03-27 11:57:40 -0500

    Update script.js

--- HW11/scripts/script.js
+++ HW11/scripts/script.js
@@ -1,18 +1,21 @@
- let holiday = {
-                "name": "Easter Sunday",
-                "date": "Variable (March/April)",
-                "type": "Religious"
-            }
-        
-        $(function () {
-            $("button").click(function () {
-                showHolidayInfo();
-            });
+let holiday = {
+    "name": "Easter Sunday",
+    "date": "Variable (March/April)",
+    "type": "Religious"
+};
 
-        });
-       
-        function showBikeInfo()
-            {
-                $("#HolidayInformation").html("Name: " + holiday.name
-                + "<br>date:" + holiday.date+ "<br>type" + holiday.type);
-            }
+$(function () {
+    $("button").click(function () {
+        // This is looking for showHolidayInfo
+        showHolidayInfo(); 
+    });
+});
+
+// Rename this from showBikeInfo to showHolidayInfo
+function showHolidayInfo() {
+    $("#HolidayInformation").html(
+        "Name: " + holiday.name + 
+        "<br>Date: " + holiday.date + 
+        "<br>Type: " + holiday.type
+    );
+}
