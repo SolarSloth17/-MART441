81a70ff3080b41c203f352b6b6d37482479c561b
Laura FitzSimons <ltyf98@gmail.com>
2026-03-27 19:09:20 -0500

    Update jsonajax.js

--- HW11.2/scripts/jsonajax.js
+++ HW11.2/scripts/jsonajax.js
@@ -1,1 +1,17 @@
+$(document).ready(function () {
+            $("button").click(function () {
+               $("#holidayInformation").load("data/holidays.json", function(responseText){
+                    var holiday = JSON.parse(responseText);
+                 
+              $.each(data.holidays, function (index, holiday) {
+              
+                htmlContent += "<div class='holiday-card'>";
+                htmlContent += "<h3>" + holiday.name + "</h3>";
+                htmlContent += "<p class='date'> " + holiday.date + "</p>";
+                htmlContent += "<span class='tag'>" + holiday.type + "</span>";
+                htmlContent += "</div>";
 
+                   $("#holidayInformation").hide().html(htmlContent).fadeIn(1000);
+                });
+            });
+        });
