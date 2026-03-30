27bc056765d904a2ceb6fcd3131873f6f77a29b4
Laura FitzSimons <ltyf98@gmail.com>
2026-03-30 13:12:22 -0500

    Update scripts.js

--- HW11.2/scripts/scripts.js
+++ HW11.2/scripts/scripts.js
@@ -1,1 +1,15 @@
+$("#holidayInformation").hide().html(htmlContent).fadeIn(1000);
 
+$(".holiday-card").each(function() {
+    let type = $(this).find(".tag").text();
+    let highlightColor = "#007bff"; 
+
+    if (type === "Federal") highlightColor = "#e63946"; 
+    if (type === "Shopping") highlightColor = "#4cc9f0";
+    if (type === "Religious") highlightColor = "#fee440"; 
+    if (type === "Observance") highlightColor = "#9b59b6"; 
+
+    $(this).highlightHoliday({
+        borderColor: highlightColor
+    });
+});
